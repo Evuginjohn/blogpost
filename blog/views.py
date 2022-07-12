a8f77ffc7ff0d0b935b7164742204cd08b921537
evugin <evugin@expertzlab.com>
2022-07-12 21:09:39 +0530

    adding

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+class BlogHome(generic.ListView):
+    model = Post
+    template_name = 'home.html'
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+# Create your views here.
